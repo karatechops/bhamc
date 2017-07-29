154377858d141a2623252688e1d5cff990793860
karatechops <contact@lazers.tv>
2017-07-29 11:59:56 -0400

    Add full browser height page layout.

--- app/src/containers/RootApp/index.js
+++ app/src/containers/RootApp/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React, { Component, PropTypes } from 'react';
 import GrommetApp from 'grommet/components/App';
+import Box from 'grommet/components/Box';
 import { AppContainer as ReactHotLoader } from 'react-hot-loader';
 import { Nav, PageMarquee, Footer } from 'components';
 
@@ -9,11 +10,22 @@ class RootApp extends Component {
   render() {
     return (
       <ReactHotLoader>
-        <GrommetApp centered={false}>
+        <GrommetApp
+          centered={false}
+          style={{
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
           <Nav />
-          <PageMarquee />
-          {this.props.children}
-          <Footer />
+          <Box flex style={{ minHeight: '100%' }}>
+            <PageMarquee />
+            <Box flex>
+              {this.props.children}
+            </Box>
+            <Footer />
+          </Box>
         </GrommetApp>
       </ReactHotLoader>
     );
