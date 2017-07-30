47fa56dfd8c2cac01ca5a1e6a2b64473770e1e89
karatechops <contact@lazers.tv>
2017-07-30 13:27:55 -0400

    Update header Nav styling.

--- app/src/components/Nav/styles.js
+++ app/src/components/Nav/styles.js
@@ -20,9 +20,28 @@ export const MobileNav = styled(Box)`
 `;
 
 export const Header = styled(GrommetHeader)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(56, 66, 78, 0);
   flex-direction: column;
   text-align: center;
+  padding: 0;
+  height: auto;
+  min-height: auto;
+  
+  .grommetux-heading {
+    display: none;
+  }
+
   @media(min-width: 880px) {
     flex-direction: row;
+    padding: 12px;
+    min-height: 72px;
+    background: rgba(56, 66, 78, 0.85);
+
+    .grommetux-heading {
+      display: block;
+    }
   }
 `;
